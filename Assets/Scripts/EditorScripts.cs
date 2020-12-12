d2203bd8a41cc26bc9e220a22908546df6093524
Stepan <cntg4br2000@mail.ru>
2020-12-12 23:29:01 +0500

    CubePosition

---
+++ Assets/Scripts/EditorScripts.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class EditorScripts : MonoBehaviour
+{
+
+    [SerializeField] [Range(1f, 20f)] private int gridSize;
+    // Update is called once per frame
+    void Update()
+    {
+        
+        Vector3 snapPos; ;
+        var snapPosition = transform.position;
+        
+        snapPos.x = Mathf.RoundToInt(snapPosition.x / 10f) * gridSize;
+        snapPos.z = Mathf.RoundToInt(snapPosition.z / 10f) * gridSize;
+        
+        snapPosition = new Vector3(snapPos.x, 0f, snapPos.z);
+        transform.position = snapPosition;
+    }
+}
